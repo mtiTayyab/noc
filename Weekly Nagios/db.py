0e6537d7eb569a02ced51fd4d63fd69d16cea087
Tayyab Iqbal <tayyab.iqbal@synavos.com>
2020-07-01 10:08:54 +0500

    Added Expresso Senegal under OPS Accra team.

--- Weekly Nagios/db.py
+++ Weekly Nagios/db.py
@@ -34,7 +34,7 @@ def store_all_data(data):
 
     lahore = ['mtn_yemen', 'mtn_afghanistan', 'mtn_syria', 'glo_nigeria', 'starlink_qatar', 'newco_bahamas', 'mtn_sudan', 'sdt_ethiopia']
     kolkata = ['gosoft_thailand', 'dna_finland', 'se_bank_system']
-    accra = ['mtn_congo', 'mtn_ghana', 'mtn_south_sudan', 'mtn_benin', 'glo_benin', 'mtn_zambia', 'mtn_ivory_coast', 'mtn_bissau', 'glo_ghana', 'swazi_mobile', 'mtn_rwanda', 'mtn_nigeria']
+    accra = ['mtn_congo', 'mtn_ghana', 'mtn_south_sudan', 'mtn_benin', 'glo_benin', 'mtn_zambia', 'mtn_ivory_coast', 'mtn_bissau', 'glo_ghana', 'swazi_mobile', 'mtn_rwanda', 'mtn_nigeria', 'expresso_senegal']
     team = ''
     for key in data:
         if lahore.__contains__(key[1].lower()):
